efba620774a5a06d39f12ecfc7373de85bb830d9
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-09-12 23:11:40 +0200

    Add project files.

---
+++ Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Models
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> opts): base(opts) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+    }
+}
